ab76e73efd4e9e9d2b122281128c4fa442010f19
NOE CAZAREZ CAMARGO <ncazarez@eca.uson.mx>
2017-03-15 10:14:26 -0700

    Se pone el firlto para depts

--- CrystalReportV2/CrystalReportV2/frmReporte.Designer.cs
+++ CrystalReportV2/CrystalReportV2/frmReporte.Designer.cs
@@ -28,11 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbDepartamento = new System.Windows.Forms.ComboBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOMBRE";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(290, 35);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(385, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DEPARTAMENTO";
+            // 
+            // cmbDepartamento
+            // 
+            this.cmbDepartamento.FormattingEnabled = true;
+            this.cmbDepartamento.Location = new System.Drawing.Point(16, 33);
+            this.cmbDepartamento.Name = "cmbDepartamento";
+            this.cmbDepartamento.Size = new System.Drawing.Size(268, 24);
+            this.cmbDepartamento.TabIndex = 3;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(681, 33);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(105, 23);
+            this.btnGenerar.TabIndex = 4;
+            this.btnGenerar.Text = "GENERAR";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            // 
+            // frmReporte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 515);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.cmbDepartamento);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmReporte";
             this.Text = "frmReporte";
+            this.Load += new System.EventHandler(this.frmReporte_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbDepartamento;
+        private System.Windows.Forms.Button btnGenerar;
     }
 }
--- CrystalReportV2/CrystalReportV2/frmReporte.cs
+++ CrystalReportV2/CrystalReportV2/frmReporte.cs
@@ -8,13 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using CrystalReportV2.Controladores;
+
 namespace CrystalReportV2
 {
     public partial class frmReporte : Form
     {
+        public void CargarDeptos() {
+            List<String> deptos= ProductoManager.getListaDepartamentos();
+            cmbDepartamento.DataSource = deptos;
+        }
+
         public frmReporte()
         {
             InitializeComponent();
+        }
+
+        private void frmReporte_Load(object sender, EventArgs e)
+        {
+            CargarDeptos();
+            cmbDepartamento.SelectedIndex = cmbDepartamento.Items.Count-1;
         }
     }
 }
